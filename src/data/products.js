2db68c5e8ed49585142d3b8cbd44f134a95624f0
soyoung <soyeongan4@gmail.com>
2026-04-16 18:07:54 +0900

    상품데이터  brand, image 추가

--- src/data/products.js
+++ src/data/products.js
@@ -1,19 +1,25 @@
-//상품 데이터(임시)
+//상품 데이터
 const products =[
     {
         id: 1,
+        brand: "MUSIT",
         name: "오버핏 반팔티",
         price: 19900,
+        image: "https://placehold.co/300x360?text=T-SHIRT",
     },
     {
         id: 2,
+        brand: "MUSIT",
         name: "와이드 데님 팬츠",
         price: 39000,
+        image: "https://placehold.co/300x360?text=DENIM",
     },
     {
-        id: 1,
+        id: 3,
+        brand: "MUSIT",
         name: "스니커즈",
         price: 59000,
+        image: "https://placehold.co/300x360?text=SNEAKERS",
     },
 ];
 
