57bc5d45c845f7dc3b05262cadbb9b8f67549dd9
soyoung <soyeongan4@gmail.com>
2026-04-16 17:33:51 +0900

    products

---
+++ src/data/products.js
@@ -0,0 +1,20 @@
+//상품 데이터(임시)
+const products =[
+    {
+        id: 1,
+        name: "오버핏 반팔티",
+        price: 19900,
+    },
+    {
+        id: 2,
+        name: "와이드 데님 팬츠",
+        price: 39000,
+    },
+    {
+        id: 1,
+        name: "스니커즈",
+        price: 59000,
+    },
+];
+
+export default products;
