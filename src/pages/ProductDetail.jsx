4211f6d36b39ae31823baa96e093e59232f3f0c4
soyoung <soyeongan4@gmail.com>
2026-04-16 19:05:54 +0900

    상세페이지 생성

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,17 @@
+//삼품 페이지
+function ProductDetail() {
+    return(
+        <div style={StyleSheet.container}>
+            <h1>상품 상세페이지</h1>
+            <p>나중에 여기에 상품 정보가 들어올 예정</p>
+        </div>
+    );
+}
+
+const styles = {
+    container: {
+        padding: "40px",
+    },
+};
+
+export default ProductDetail;
