2434e6ab38f95ba6a446d30bac3cbc2b0d42d78e
soyoung <soyeongan4@gmail.com>
2026-04-19 18:42:27 +0900

    카테고리 필터 기능 추가

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,23 +1,56 @@
 //Home 재사용
 // 상품 목록 페이지
+import { useState } from "react";
 import products from "../data/products";
 import ProductCard from "../components/ProductCard";
 
 function Products() {
+    //현재 선택된 카테고리 저장
+    //값을 바꾸는 함수 = setSelectedCategory
+    //처음 값 = "전체"
+    const [selectedCategory, setSelectedCategory] = useState("전체");
+
+    //선택된 카테고리에 따라 상품 걸러주기
+    //전체 = 다 보여줌
+    //아니면 카테고리 같은 것만 남김
+    const filteredProducts =
+    selectedCategory === "전체"
+        ? products
+        : products.filter((item) => item.category === selectedCategory);
+   
     return (
         <div style={styles.container}>
         <h1 style={styles.title}>전체 상품</h1>
-        {/* 카테고리 바*/}
+        {/* 카테고리 버튼*/}
         <div style={styles.categoryBar}>
-            <button style={styles.categoryButton}>전체</button>
-            <button style={styles.categoryButton}>상의</button>
-            <button style={styles.categoryButton}>하의</button>
-            <button style={styles.categoryButton}>신발</button>
-            <button style={styles.categoryButton}>가방</button>
+            <button
+                style={styles.categoryButton}
+                onClick={() => setSelectedCategory("전체")}>전체
+            </button>
+            <button
+                style={styles.categoryButton}
+                onClick={() => setSelectedCategory("상의")}>상의
+            </button>
+            <button
+                style={styles.categoryButton}
+                onClick={() => setSelectedCategory("하의")}>하의
+            </button>
+            <button
+                style={styles.categoryButton}
+                onClick={() => setSelectedCategory("신발")}>신발
+            </button>
+            <button
+                style={styles.categoryButton}
+                onClick={() => setSelectedCategory("가방")}>가방
+            </button> 
         </div>
-
+        {/* 선택된 카테고리 표시 */}
+        <p style={styles.categoryText}>
+            현재 선택: {selectedCategory}
+        </p>
+        {/* 상품 목록 */}
         <div style={styles.productList}>
-            {products.map((item) => (
+            {filteredProducts.map((item) => (
                 <ProductCard
                     key={item.id}
                     id={item.id}
@@ -50,6 +83,10 @@ const styles = {
     border: "1px solid #ddd",
     backGroundColor: "#fff",
     curor: "pointer",
+  },
+  categoryText: {
+    marginBottom: "20px",
+    color: "#666",
   },
   productList: {
     display: "grid",
